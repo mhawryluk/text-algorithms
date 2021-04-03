a09f46a2a06fed41dc4505f5284d108f13ffdb38
mhawryluk <marcinhawrylukxx@gmail.com>
2021-04-03 19:49:30 +0200

    lab 2 - implementation part done
    suffix trees completed,
    created jupyter notebook

--- 2-suffix_trees/trie_and_tree.py
+++ 2-suffix_trees/trie_and_tree.py
@@ -1,7 +1,7 @@
 from treelib import Tree
 
 
-class Node:
+class TrieNode:
     def __init__(self, char):
         self.char = char
         self.children = {}
@@ -13,9 +13,30 @@ class Node:
         return self.char < other.char
 
 
+class TreeNode:
+    def __init__(self, range):
+        self.range = range
+        self.children = set()
+
+    def __str__(self):
+        str = ""
+        for i in range(self.range[0], self.range[1]+1):
+            str += TreeNode.text[i]
+        return str
+
+    def __repr__(self):
+        str = ""
+        for i in range(self.range[0], self.range[1]+1):
+            str += TreeNode.text[i]
+        return str
+
+    def __lt__(self, other):
+        return str(self) < str(other)
+
+
 def build_trie(text):
-    text += '\0'
-    head = Node("")
+    text += '\$'
+    head = TrieNode("")
 
     for i in range(len(text)):
         current_node = head
@@ -24,9 +45,55 @@ def build_trie(text):
                 current_node = current_node.children[char]
             else:
                 for k, char in enumerate(text[i+j:]):
-                    new_node = Node(char)
+                    new_node = TrieNode(char)
                     current_node.children[char] = new_node
                     current_node = new_node
+                break
+
+    return head
+
+
+def build_tree(text):
+    text += '$'
+    head = TreeNode(range=(0, -1))
+
+    for suffix_index in range(len(text)):
+        current_node = head
+        current_index = suffix_index
+        found = 0
+        while current_index < len(text) and not found:
+            skip = 0
+            for child in current_node.children:
+                common = 0
+
+                for k in range(child.range[0], child.range[1]+1):
+                    if text[suffix_index+common] == text[k]:
+                        common += 1
+                    else:
+                        break
+
+                if common == child.range[1] - child.range[0] + 1:
+                    current_node = child
+                    current_index += common
+                    skip = 1
+
+                elif common > 0:
+                    new_child_1 = TreeNode(
+                        range=(child.range[0]+common, child.range[1]))
+                    new_child_2 = TreeNode(
+                        range=(current_index+common, len(text)-1))
+
+                    new_child_1.children = child.children
+
+                    child.range = (
+                        child.range[0], child.range[0]+common-1)
+                    child.children = {new_child_1, new_child_2}
+                    found = 1
+                    break
+
+            if not found and not skip:
+                new_child = TreeNode(range=(current_index, len(text)-1))
+                current_node.children.add(new_child)
                 break
 
     return head
@@ -36,7 +103,6 @@ def show_trie(text):
     tree = Tree()
     head = build_trie(text)
     tree.create_node(head, head, parent=None)
-    tree.show()
 
     def create_tree(node):
         for child in node.children.values():
@@ -47,9 +113,21 @@ def show_trie(text):
     tree.show()
 
 
-def build_suffix_tree(text):
-    pass
+def show_tree(text, head):
+    TreeNode.text = text+"$"
+    tree = Tree()
+    tree.create_node(head, head, parent=None)
+
+    def create_tree(node):
+        for child in node.children:
+            tree.create_node(child, child, parent=node)
+            create_tree(child)
+
+    create_tree(head)
+    tree.show()
 
 
 if __name__ == "__main__":
-    show_trie("aabbabbc")
+    text = ""
+    head = build_tree(text)
+    show_tree(text, head)
