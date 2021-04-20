f667431e9bf807ba3d47f01d7dd39ca2e2cd09b3
mhawryluk <marcinhawrylukxx@gmail.com>
2021-04-21 01:12:42 +0200

    lab 3 - static huffman code done

--- 3-text-compression/huffman.py
+++ 3-text-compression/huffman.py
@@ -2,6 +2,9 @@ from collections import defaultdict
 from treelib import Tree
 from heapq import heappush, heappop
 from collections import Counter
+from bitarray import bitarray, decodetree
+from bitarray.util import *
+import os
 
 
 class Node:
@@ -61,6 +64,81 @@ def huffman_heap(letter_counts):
     return heappop(heap)
 
 
+def get_codes(head):
+    codes = {}
+    head.code = ''
+
+    def walk_tree(node):
+        for i, child in enumerate(node.elements):
+            child.code = node.code + str(i)
+
+            if len(child.elements) > 1:
+                walk_tree(child)
+            else:
+                codes[child.elements[0]] = bitarray(child.code)
+
+    walk_tree(head)
+    return codes
+
+
+def encode(text, file):
+    node = huffman(Counter(text))
+    codes = get_codes(node)
+    encoded_text = bitarray()
+    encoded_text.encode(codes, text)
+
+    mapping = bitarray()
+
+    for letter, code in codes.items():
+        letter_utf = bitarray()
+        letter_utf.frombytes(letter.encode('utf-32'))
+        # print(letter, letter_utf)
+
+        code_len = bitarray()
+        code_len.frombytes(len(code).to_bytes(1, 'big'))
+        mapping += letter_utf + code_len + code
+
+    letters_count = bitarray()
+    letters_count.frombytes(len(codes).to_bytes(4, 'big'))
+    text_bit_size = bitarray()
+    text_bit_size.frombytes(len(encoded_text).to_bytes(4, 'big'))
+
+    bit_seq = bitarray()
+    bit_seq = letters_count + mapping + text_bit_size + encoded_text
+
+    with open(file, 'wb') as f:
+        bit_seq.tofile(f)
+
+
+def decode(file):
+    with open(file, 'rb') as f:
+        bit_seq = bitarray()
+        bit_seq.fromfile(f)
+
+    letters_count = ba2int(bit_seq[:32])
+    decode_dict = {}
+    i = 32
+
+    for _ in range(letters_count):
+        # print(bit_seq[i:i+64])
+        letter = bit_seq[i:i+64].tobytes().decode('utf-32')
+        # print(letter)
+        i += 64
+        code_len = ba2int(bit_seq[i:i+8])
+        i += 8
+        code = bit_seq[i:i+code_len]
+        i += code_len
+
+        decode_dict[letter] = code
+
+    text_len = ba2int(bit_seq[i:i+32])
+    i += 32
+
+    decode_tree = decodetree(decode_dict)
+    text = ''.join(bit_seq[i:i+text_len].decode(decode_tree))
+    return text
+
+
 def show_tree(head):
     tree = Tree()
     tree.create_node(str(head) + ' weight: ' +
@@ -79,29 +157,11 @@ def show_tree(head):
     tree.show()
 
 
-def get_codes(head):
-    codes = {}
-    head.code = ''
-
-    def walk_tree(node):
-        for i, child in enumerate(node.elements):
-            child.code = node.code + str(i)
-
-            if len(child.elements) > 1:
-                walk_tree(child)
-            else:
-                codes[child.elements[0]] = child.code
-
-    walk_tree(head)
-    return codes
-
-
 # def adaptive_huffman(text):
 #     Node.nodes = []
 #     count = defaultdict(int)
 #     nodes = {"#": Node("#", weight=0)}
 #     root = nodes["#"]
-
 #     for letter in list(text):
 #         if letter in nodes:
 #             node = nodes[letter]
@@ -120,6 +180,8 @@ def get_codes(head):
 #             nodes["#"] = zero_node
 #             updated_node.increment()
 
-
 if __name__ == '__main__':
-    print(get_codes(huffman_heap(Counter('abracadabra'))))
+    text = 'żółć'
+    encode(text, 'encoded')
+    print(decode('encoded'))
+    print(os.path.getsize('encoded'))
