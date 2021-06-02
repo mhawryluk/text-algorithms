52aca781e8ade9c42fd3354c28224e6a35b150e8
mhawryluk <marcinhawrylukxx@gmail.com>
2021-06-03 00:10:26 +0200

    lab 6 - 2d pattern matching implemented

--- 6-2d-matching/2d-matching.py
+++ 6-2d-matching/2d-matching.py
@@ -35,41 +35,17 @@ def build_trie(patterns):
                 current_node.edges[char] = Node(char=char, parent=current_node)
                 current_node = current_node.edges[char]
 
-        current_node.pattern_end = i
-
-    return head
-
-
-def modofy_automaton(head):
-
-    queue = [head]
-
-    def assign_fail_links(node):
-        if node is not head:
-            current_node = node.parent.fail_link
-            while current_node:
-                if node.char in current_node.edges:
-                    node.fail_link = current_node.edges[node.char]
-                    break
-                current_node = current_node.fail_link
-            else:
-                node.fail_link = head
-
-        for edge in node.edges.values():
-            if edge not in queue:
-                queue.append(edge)
-
-    while queue:
-        node = queue.pop(0)
-        assign_fail_links(node)
+        current_node.pattern_end = i+1
 
     return head
 
 
 def build_automaton(patterns):
     head = build_trie(patterns)
+    # show_graph(head)
 
     queue = [head]
+    visited = []
 
     def assign_fail_links(node):
         if node is not head:
@@ -83,8 +59,10 @@ def build_automaton(patterns):
                 node.fail_link = head
 
         for edge in node.edges.values():
-            if edge not in queue:
+            if edge not in visited:
                 queue.append(edge)
+
+        visited.append(node)
 
     while queue:
         node = queue.pop(0)
@@ -93,8 +71,66 @@ def build_automaton(patterns):
     return head
 
 
+def modify_automaton(head):
+
+    queue = [head]
+    visited = []
+
+    def copy_edges(node):
+        if node is not head:
+            for key, node_edge in node.fail_link.edges.items():
+                if key not in node.edges:
+                    node.edges[key] = node_edge
+
+        for edge in node.edges.values():
+            if edge not in visited and edge not in queue:
+                queue.append(edge)
+                visited.append(edge)
+
+        visited.append(node)
+        node.fail_link = None
+
+    while queue:
+        node = queue.pop(0)
+        copy_edges(node)
+
+    return head
+
+
 def pattern_match_2d(text, pattern):
-    head = build_automaton(text, pattern)
+    head = build_automaton(pattern)
+    # show_graph(head)
+    head = modify_automaton(head)
+    # show_graph(head)
+
+    patterns_matrix = [[0 for _ in range(len(text[0]))]
+                       for _ in range(len(text))]
+
+    for i, line in enumerate(text):
+        current_node = head
+        for j, char in enumerate(line):
+            if char in current_node.edges:
+                current_node = current_node.edges[char]
+            else:
+                current_node = head
+
+            if current_node.pattern_end:
+                patterns_matrix[i][j] = current_node.pattern_end
+
+    print(patterns_matrix)
+
+    col_pattern = [i+1 for i in range(len(pattern[0]))]
+
+    result = []
+
+    for i in range(len(text[0])):
+        line = [patterns_matrix[j][i] for j in range(len(text))]
+        print(line, col_pattern)
+        for j in kmp(line, col_pattern):
+            result.append((j, i-len(pattern[0])+1))
+
+    print(result)
+    return result
 
 
 def show_trie(patterns=None, head=None):
@@ -125,11 +161,7 @@ def show_graph(head):
                 stack.append(edge_node)
 
         if node.fail_link:
-            if node.fail_link in graph[node]:
-                graph.add_edge(
-                    node, node.fail_link, label=graph[node][node.fail_link].label + 'faillink')
-            else:
-                graph.add_edge(node, node.fail_link)
+            graph.add_edge(node, node.fail_link)
 
         node.visited = True
 
@@ -141,7 +173,50 @@ def show_graph(head):
     plt.show()
 
 
+def kmp(text, pattern):
+    result = []
+
+    pi = prefix_function(pattern)
+    q = 0
+
+    for i in range(0, len(text)):
+        while q > 0 and pattern[q] != text[i]:
+            q = pi[q-1]
+
+        if pattern[q] == text[i]:
+            q = q + 1
+
+        if q == len(pattern):
+            result.append(i + 1 - q)
+            q = pi[q-1]
+
+    return result
+
+
+def prefix_function(pattern):
+    pi = [0]
+    k = 0
+
+    for q in range(1, len(pattern)):
+        while(k > 0 and pattern[k] != pattern[q]):
+            k = pi[k-1]
+
+        if(pattern[k] == pattern[q]):
+            k = k + 1
+
+        pi.append(k)
+
+    return pi
+
+
 if __name__ == '__main__':
-    # head = build_trie(['abc', 'aab', 'cba'])
-    head = build_automaton(['abc', 'aab', 'cba'])
-    show_graph(head)
+    # # head = build_trie(['abc', 'aab', 'cba'])
+    # head = build_automaton(['abc', 'aab', 'cba'])
+    # # show_graph(head)
+    # head = modify_automaton(head)
+    # show_graph(head)
+
+    text = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9, 9, 9]]
+    pattern = [[1, 2], [4, 5]]
+
+    pattern_match_2d(text, pattern)
